3b7b1d03bf673998d83faa6c7fb038bd4c338339
Diego Dominguez <diegodominguez333@gmail.com>
2020-11-30 14:14:09 -0700

    need a state mngr

--- src/components/SortData.js
+++ src/components/SortData.js
@@ -8,6 +8,7 @@ class SortData extends React.Component {
     super(props); 
     this.state = {
       elements: [],
+      selectedAlgorithm: 'bubble', 
       elementsSize: 10,
       minValue: 1, 
       maxValue: 100
@@ -21,6 +22,10 @@ class SortData extends React.Component {
     this.setState({elements: this.populateElementsList()}); 
   }
 
+  componentDidUpdate() {
+    this.bubbleSort();
+  }
+
   getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
   }
@@ -32,6 +37,32 @@ class SortData extends React.Component {
       tempList.push(randomNum); 
     }
     return  tempList; 
+  }
+
+  bubbleSort() {
+
+    let isSorted = false;
+    let count = 0;
+    let array = this.state.elements; 
+    const swap = (index1, index2, array) =>  {
+      const temp = array[index1];
+      array[index1] = array[index2];
+      array[index2] = temp;
+    }
+    while (!isSorted) {
+      isSorted = true;
+      for (let i = 0; i < array.length - 1 - count; i++) {
+        if (array[i] > array[i + 1]) { 
+          setTimeout(() => {
+            console.log(array);
+            this.setState({elements: [...array]});
+          },500);
+          isSorted = false;
+          swap(i, i + 1, array);
+        }
+      }
+      count++;
+    }
   }
 
   renderElements() {
